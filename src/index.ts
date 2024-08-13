6eee59700d3354ea6d87ee46c26f537bf3cf75b1
Thomas Aelbrecht <thomasaelbrecht@live.com>
2024-08-13 13:56:19 +0200

    Koa router en bodyparser toegevoegd

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+import Router from '@koa/router';
 import { getLogger } from './core/logging';
 
 const app = new Koa();
 
-app.use(async (ctx) => {
-  ctx.body = 'Hello World from TypeScript';
+app.use(bodyParser());
+
+const router = new Router();
+
+router.get('/api/transactions', async (ctx) => {
+  ctx.body = '[{"user": "Benjamin", "amount": 100, "place": "Irish Pub", "date": "2021-08-15" }]';
 });
+
+app.use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(9000, () => {
   getLogger().info('🚀 Server listening on http://127.0.0.1:9000');
