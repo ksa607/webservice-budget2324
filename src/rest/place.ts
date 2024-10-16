8795126b307c38fb5dce50f907e02f1aa40b5be2
Thomas Aelbrecht <thomasaelbrecht@live.com>
2024-10-16 12:12:52 +0200

    Add validation for places router

--- src/rest/place.ts
+++ src/rest/place.ts
@@ -1,4 +1,5 @@
 import Router from '@koa/router';
+import Joi from 'joi';
 import * as placeService from '../service/place';
 import * as transactionService from '../service/transaction';
 import type { BudgetAppContext, BudgetAppState} from '../types/koa';
@@ -13,6 +14,7 @@ import type {
 } from '../types/place';
 import type { IdParams } from '../types/common';
 import type { GetAllTransactionsReponse } from '../types/transaction';
+import validate from '../core/validation';
 
 const getAllPlaces = async (ctx: KoaContext<GetAllPlacesResponse>) => {
   const places = await placeService.getAll();
@@ -20,10 +22,16 @@ const getAllPlaces = async (ctx: KoaContext<GetAllPlacesResponse>) => {
     items: places,
   };
 };
+getAllPlaces.validationScheme = null;
 
 const getPlaceById = async (ctx: KoaContext<GetPlaceByIdResponse, IdParams>) => {
-  const place = await placeService.getById(Number(ctx.params.id));
+  const place = await placeService.getById(ctx.params.id);
   ctx.body = place;
+};
+getPlaceById.validationScheme = {
+  params: {
+    id: Joi.number().integer().positive(),
+  },
 };
 
 const createPlace = async (ctx: KoaContext<CreatePlaceResponse, void, CreatePlaceRequest>) => {
@@ -31,22 +39,45 @@ const createPlace = async (ctx: KoaContext<CreatePlaceResponse, void, CreatePlac
   ctx.status = 201;
   ctx.body = place;
 };
+createPlace.validationScheme = {
+  body: {
+    name: Joi.string().max(255),
+    rating: Joi.number().integer().min(1).max(5).optional(),
+  },
+};
 
 const updatePlace = async (ctx: KoaContext<UpdatePlaceResponse, IdParams, UpdatePlaceRequest>) => {
-  const place = await placeService.updateById(Number(ctx.params.id), ctx.request.body);
+  const place = await placeService.updateById(ctx.params.id, ctx.request.body);
   ctx.body = place;
+};
+updatePlace.validationScheme = {
+  params: { id: Joi.number().integer().positive() },
+  body: {
+    name: Joi.string().max(255),
+    rating: Joi.number().integer().min(1).max(5),
+  },
 };
 
 const deletePlace = async (ctx: KoaContext<void, IdParams>) => {
-  await placeService.deleteById(Number(ctx.params.id));
+  await placeService.deleteById(ctx.params.id);
   ctx.status = 204;
+};
+deletePlace.validationScheme = {
+  params: {
+    id: Joi.number().integer().positive(),
+  },
 };
 
 const getTransactionsByPlaceId = async (ctx: KoaContext<GetAllTransactionsReponse, IdParams>) => {
-  const transactions = await transactionService.getTransactionsByPlaceId(Number(ctx.params.id));
+  const transactions = await transactionService.getTransactionsByPlaceId(ctx.params.id);
   ctx.body = {
     items: transactions,
   };
+};
+getTransactionsByPlaceId.validationScheme = {
+  params: {
+    id: Joi.number().integer().positive(),
+  },
 };
 
 export default (parent: KoaRouter) => {
@@ -54,13 +85,13 @@ export default (parent: KoaRouter) => {
     prefix: '/places',
   });
 
-  router.get('/', getAllPlaces);
-  router.get('/:id', getPlaceById);
-  router.post('/', createPlace);
-  router.put('/:id', updatePlace);
-  router.delete('/:id', deletePlace);
+  router.get('/', validate(getAllPlaces.validationScheme), getAllPlaces);
+  router.get('/:id', validate(getPlaceById.validationScheme), getPlaceById);
+  router.post('/', validate(createPlace.validationScheme), createPlace);
+  router.put('/:id', validate(updatePlace.validationScheme), updatePlace);
+  router.delete('/:id', validate(deletePlace.validationScheme), deletePlace);
 
-  router.get('/:id/transactions', getTransactionsByPlaceId);
+  router.get('/:id/transactions', validate(getTransactionsByPlaceId.validationScheme), getTransactionsByPlaceId);
 
   parent.use(router.routes())
     .use(router.allowedMethods());
