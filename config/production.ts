246da9fbf070ce4aec6600bfb93105b1e6c3c803
Thomas Aelbrecht <thomasaelbrecht@live.com>
2024-10-23 13:54:39 +0200

    Add jsonwebtoken and JWT config

--- config/production.ts
+++ config/production.ts
@@ -7,4 +7,12 @@ export default {
     origins: ['http://localhost:5173'],
     maxAge: 3 * 60 * 60,
   },
+  auth: {
+    jwt: {
+      audience: 'budget.hogent.be',
+      issuer: 'budget.hogent.be',
+      expirationInterval: 7 * 24 * 60 * 60, // s (7 days)
+      secret: 'eenveeltemoeilijksecretdatniemandooitzalradenandersisdesitegehacked',
+    },
+  },
 };
--- config/testing.ts
+++ config/testing.ts
@@ -7,4 +7,12 @@ export default {
     origins: ['http://localhost:5173'],
     maxAge: 3 * 60 * 60,
   },
+  auth: {
+    jwt: {
+      audience: 'budget.hogent.be',
+      issuer: 'budget.hogent.be',
+      expirationInterval: 60 * 60, // s (1 hour)
+      secret: 'eenveeltemoeilijksecretdatniemandooitzalradenandersisdesitegehacked',
+    },
+  },
 };
