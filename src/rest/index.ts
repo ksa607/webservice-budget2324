81ced0db39d0e11bb2490d6ca2cd37b256b955ec
Thomas Aelbrecht <thomasaelbrecht@live.com>
2024-11-05 15:07:17 +0100

    Add base components

--- src/rest/index.ts
+++ src/rest/index.ts
@@ -6,6 +6,42 @@ import installUserRouter from './user';
 import installSessionRouter from './session';
 import type { BudgetAppContext, BudgetAppState, KoaApplication } from '../types/koa';
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Base:
+ *       required:
+ *         - id
+ *       properties:
+ *         id:
+ *           type: integer
+ *           format: "int32"
+ *   parameters:
+ *     idParam:
+ *       in: path
+ *       name: id
+ *       description: Id of the item to fetch/update/delete
+ *       required: true
+ *       schema:
+ *         type: integer
+ *         format: "int32"
+ *   securitySchemes:
+ *     bearerAuth: # arbitrary name for the security scheme
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT # optional, arbitrary value for documentation purposes
+ *   responses:
+ *     400BadRequest:
+ *       description: You provided invalid data
+ *     401Unauthorized:
+ *       description: You need to be authenticated to access this resource
+ *     403Forbidden:
+ *       description: You don't have access to this resource
+ *     404NotFound:
+ *       description: The requested resource could not be found
+ */
+
 export default (app: KoaApplication) => {
   const router = new Router<BudgetAppState, BudgetAppContext>({
     prefix: '/api',
