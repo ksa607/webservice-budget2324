823fb1687a9bc269fa27970fb98d2c9e5b62fadd
Thomas Aelbrecht <thomasaelbrecht@live.com>
2024-11-05 16:30:54 +0100

    Add health endpoints docs

--- src/rest/health.ts
+++ src/rest/health.ts
@@ -5,12 +5,69 @@ import type { KoaContext, KoaRouter } from '../types/koa';
 import type { PingResponse, VersionResponse } from '../types/health';
 import validate from '../core/validation';
 
+/**
+ * @swagger
+ * tags:
+ *   name: Health
+ *   description: Health check endpoints
+ */
+
+/**
+ * @swagger
+ * /api/health/ping:
+ *   get:
+ *     summary: Ping the server
+ *     tags:
+ *      - Health
+ *     responses:
+ *       200:
+ *         description: Server pongs back
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               required:
+ *                 - pong
+ *               properties:
+ *                 pong:
+ *                   type: boolean
+ *       400:
+ *         $ref: '#/components/responses/400BadRequest'
+ */
 const ping = async (ctx: KoaContext<PingResponse>) => {
   ctx.status = 200;
   ctx.body = healthService.ping();
 };
 ping.validationScheme = null;
 
+/**
+ * @swagger
+ * /api/health/version:
+ *   get:
+ *     summary: Get the server's version information
+ *     tags:
+ *      - Health
+ *     responses:
+ *       200:
+ *         description: The server's running version information
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               required:
+ *                 - env
+ *                 - version
+ *                 - name
+ *               properties:
+ *                 env:
+ *                   type: string
+ *                 version:
+ *                   type: string
+ *                 name:
+ *                   type: string
+ *       400:
+ *         $ref: '#/components/responses/400BadRequest'
+ */
 const getVersion = async (ctx: KoaContext<VersionResponse>) => {
   ctx.status = 200;
   ctx.body = healthService.getVersion();
